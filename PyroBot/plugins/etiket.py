f497181c63d91b6263b1c906642e24304594aa10
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-15 09:14:20 +0300

    Create etiket.py

---
+++ PyroBot/plugins/etiket.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, filters
+import asyncio
+from pyrogram import TelegramClient, sync, events, utils, types, Button
+from pyrogram.events import StopPropagation 
+
+@Client.on_message(filters.command(["basla"]))
+async def start(bot, update):
+
+chat_id=update.chat.id,
+
+assert Client.start()
+
+if not client.is_user_authorized():
+     client.send_code_request(BOT_TOKEN)
+
+for member in app.iter_chat_members("chat_id"):
+    if not user.bot:
+        if user.username != None:
+            print(user.username)
+            Mention.append(user.username)
+        	
+for etiket in Mention:
+    await bot.send_message(event.chat_id, f"@{etiket} {reason}")
+    Mention.remove(etiket)
+    await sleep(0.5)
+
+@Client.on_message(filters.command(["start"]))
+async def deneme(bot, update):
+
+assert Client.stop()
