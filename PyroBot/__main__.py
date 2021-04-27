d4df64eb70b7cb606753a1bd6019105244f02cc8
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-27 17:05:19 +0300

    Update __main__.py

--- PyroBot/__main__.py
+++ PyroBot/__main__.py
@@ -7,9 +7,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # vars
+LOGGER = True
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
+COMMAND_HAND_LER = config("COMMAND_HAND_LER", default=None)
 
 if __name__ == "__main__" :
     print("Starting Bot...")
